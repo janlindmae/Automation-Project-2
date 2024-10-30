3df544df3278593b957cd163e9a6ca0e7720cbe5
janlindmae <jan.lindmae@gmail.com>
2024-10-30 18:21:08 +0200

    Git push for assignment 2 tasks

--- cypress/e2e/e2e-pom/issue-create-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-create-in-pom.cy.js
@@ -3,13 +3,17 @@
  */
 import IssueModal from "../../pages/IssueModal";
 
-describe('Issue create', () => {
+import { faker } from "@faker-js/faker";
+
+describe("Issue create", () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open isse creation modal  
-    cy.visit(url + '/board?modal-issue-create=true');
-    });
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .then((url) => {
+        //open isse creation modal
+        cy.visit(url + "/board?modal-issue-create=true");
+      });
   });
 
   //data set with which we are creating issue, saved as variable
@@ -21,10 +25,43 @@ describe('Issue create', () => {
   };
 
   //number of issues we expect to see in the backlog after the test
-  const EXPECTED_AMOUNT_OF_ISSUES = '5';
+  const EXPECTED_AMOUNT_OF_ISSUES = "5";
 
-  it('Should create issue successfully', () => {
+  it("Should create issue successfully", () => {
     IssueModal.createIssue(issueDetails);
     IssueModal.ensureIssueIsCreated(EXPECTED_AMOUNT_OF_ISSUES, issueDetails);
+  });
+
+  const secondIssueDetails = {
+    title: "Bug",
+    description: "My bug description",
+    type: "Bug",
+    priority: "Highest",
+    reporter: "Pickle Rick",
+    assignee: "Lord Gaben",
+  };
+
+  it("Should create second issue successfully", () => {
+    IssueModal.createIssue(secondIssueDetails);
+    IssueModal.ensureIssueIsCreated(
+      EXPECTED_AMOUNT_OF_ISSUES,
+      secondIssueDetails
+    );
+  });
+
+  const randomTitle = faker.lorem.words(1);
+  const description = faker.lorem.words(5);
+  const fakerIssue = {
+    title: randomTitle,
+    description: description,
+    type: "Task",
+    priority: "Low",
+    reporter: "Baby Yoda",
+    assignee: "Lord Gaben",
+  };
+
+  it.only("Create a third issue using faker data", () => {
+    IssueModal.createIssue(fakerIssue);
+    IssueModal.ensureIssueIsCreated(EXPECTED_AMOUNT_OF_ISSUES, fakerIssue);
   });
 });
